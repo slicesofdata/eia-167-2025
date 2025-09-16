61be0c1b74d8388d11ffeede0e840a43983c9f1e
ashivdasani26-cmc <ashivdasani26@students.claremontmckenna.edu>
2025-09-15 18:22:32 -0700

    modified comments to using starter script

---
+++ src/data/using_starter_script.R
@@ -0,0 +1,37 @@
+################################################################################
+# Script Name: using_starter_script.R
+# Author: Aleya Shivdasani 
+# GitHub:ashivdasani26-cmc
+# Date Created:09/15/2025
+#
+# Purpose: This script works on HW1. The purpose of this will stage,commit, 
+# and push file edits on your personal branch as well as merge the dev branch 
+#into your branch
+#
+################################################################################
+
+################################################################################
+# Note: When sourcing script files, if you do not want objects
+# available in this script, use the source() function along with
+# the local = TRUE argument. By default, source() will make
+# objects available in the current environment.
+
+################################################################################
+# Load necessary libraries/source any function directories
+# Example:
+#R.utils::sourceDirectory(here::here("src", "functions"))
+source(here::here("src", "functions", "load-libraries.R"))
+
+################################################################################
+# ...
+
+
+################################################################################
+# ...
+
+
+################################################################################
+# ...
+
+################################################################################
+# End of script
